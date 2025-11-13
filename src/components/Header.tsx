728800951ddc9616e4b05aa3537c33b5e70bcdd2
Alfonso Jiménez <yo@alfonsojimenez.net>
2025-11-13 07:43:14 +0100

    Soluciones modificaciones propuestas

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,13 @@
 export default function Header() {
     const secciones : string[] = ["Inicio", "Noticias", "Tutoriales", "Contacto"];
-    // Intentar generar los lis a partir del array.
   return (
     <header className="header">
       <h1 className="header__title">Blog de Desarrollo Web</h1>
       <nav className="header__nav">
         <ul>
-          <li>Inicio</li>
-          <li>Noticias</li>
-          <li>Tutoriales</li>
-          <li>Contacto</li>
+          {secciones.map((seccion) => (
+            <li key={seccion}>{seccion}</li>
+          ))}
         </ul>
       </nav>
     </header>
